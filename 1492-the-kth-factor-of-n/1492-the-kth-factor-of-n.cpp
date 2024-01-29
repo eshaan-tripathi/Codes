16cab95cd938d55fdcaa89fb762860b7d81ed5a3
Eshaan Tripathi <118004452+eshaan-tripathi@users.noreply.github.com>
2024-01-29 22:10:34 +0530

    Time: 3 ms (36.71%), Space: 7.4 MB (8.14%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector <int> result;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                result.push_back(i);
+            }
+        }
+        if(k<=result.size())
+        {
+            return result[k-1];
+        }
+        else
+            return -1;
+    }
+};
